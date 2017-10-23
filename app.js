f401ec8b51ccbfdcd7ea39fe27bc99255f0d14a0
Tobias Möhring <dasbente@gmail.com>
2017-10-23 14:28:52 +0200

    Dirty commit to change work environment

---
+++ app.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let app = express();
+
+let path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/video', function (req, res) {
+    res.render('video', {});
+});
+
+app.use(function (req, res, next) {
+    let err = new Error('Not found');
+    err.status = 404;
+    next.err;
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+
+    res.render('error', {
+	message: err.message,
+	error: app.get('env') === 'development' ? err : {}
+    });
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,29 @@
+let app = require('./app.js');
+
+// TODO normalization/ error handling
+const port = process.env.PORT || 8080;
+
+app.listen(port);
+
+app.on('listening', function () {
+    console.log('Listening on port '+ port +'!');
+});
+
+app.on('error', function (err) {
+    if (error.syscall !== 'listen') throw err;
+
+    switch (error.code) {
+	case 'EACCES':
+	    console.error(port +' requires elevated privileges');
+	    process.exit(1);
+	    break;
+
+	case 'EADDRINUSE':
+	    console.error(port +' already in use');
+	    process.exit(1);
+	    break;
+
+	default:
+	    throw err;
+    }
+});
