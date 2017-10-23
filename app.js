7ecc96c979666de8e67754dfab6306821e26b647
Tobias Möhring <dasbente@gmail.com>
2017-10-23 16:50:27 +0200

    Merged video-stream-sample code into servers codebase

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
 
-let path = require('path');
+const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,8 +10,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/video', function (req, res) {
+app.get('/', function (req, res) {
     res.render('video', {});
+});
+
+app.get('/video', function (req, res) {
+    const path = 'public/video/sample.mkv';
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.header.range;
+
+    if (range) {
+	const parts = range.replace(/bytes=/, '').split('-');
+	const start = parseInt(parts[0], 10);
+	const end = parts[1] ? parseInt([1], 10) : fileSize - 1;
+
+	const chunksize = (end - start) + 1;
+	const file = fs.createReadStream(path, {start, end});
+	let head = {
+	    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+	    'Accept-Ranges': 'bytes',
+	    'Content-Length': chunksize,
+	    'Content-Type': 'video/mkv',
+	}
+
+	res.writeHead(206, head);
+	file.pipe(res);
+    } else {
+	let head = {
+	    'Content-Length': fileSize,
+	    'Content-Type': 'video/mp4',
+	}
+
+	res.writeHead(200, head);
+	fs.createReadStream(path).pipe(res);
+    }
 });
 
 app.use(function (req, res, next) {
