3596d186eb7a1935e68dc83891b98b59606e896d
Tobias Möhring <dasbente@gmail.com>
2017-11-02 01:30:36 +0100

    Adapted renderer to use jQuery instead of DOM

--- src/videoRenderer.js
+++ src/videoRenderer.js
@@ -13,21 +13,9 @@ var mouseX = 0, mouseY = 0;
 
 var windowHalfX = window.innerWidth / 2, windowHalfY = window.innerHeight / 2;
 
-init();
-animate();
-
-function init() {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-
-    var info = document.createElement('div');
-    info.style.position = 'absolute';
-    info.style.top = '10px';
-    info.style.width = '100%';
-    info.style.textAlign = 'center';
-    info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - video demo.playing' 
-	+ '<a href="http://durian.blender.org/" target="_blank">sintel</a> trailer';
-    container.appendChild(info);
+$(function () {
+    var container = $('<div>'); // Might be unused
+    $('body').append(container);
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 1000;
@@ -50,21 +38,27 @@ function init() {
     texture.format = THREE.RGBFormat;
     texture.wrapS = THREE.ClampToEdgeWrapping;
     texture.wrapT = THREE.ClampToEdgeWrapping;
+
     var material = new THREE.MeshBasicMaterial({
         map: texture,
         overdraw: true
     });
+
     var plane = new THREE.PlaneGeometry(1280, 768, 4, 4);
+
     mesh = new THREE.Mesh(plane, material);
     scene.add(mesh);
-    //
+    
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-    //
-    window.addEventListener('resize', onWindowResize, false);
-}
+
+    container.append($(renderer.domElement));
+    $(document).on('mousemove', onDocumentMouseMove);
+    $(window).on('resize', onWindowResize);
+
+    animate();
+});
+
 function onWindowResize() {
     windowHalfX = window.innerWidth / 2;
     windowHalfY = window.innerHeight / 2;
@@ -72,15 +66,17 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 function onDocumentMouseMove(event) {
     mouseX = ( event.clientX - windowHalfX );
     mouseY = ( event.clientY - windowHalfY ) * 0.2;
 }
-//
+
 function animate() {
     requestAnimationFrame(animate);
     render();
 }
+
 function render() {
     camera.position.x += ( mouseX - camera.position.x ) * 0.05;
     camera.position.y += ( -mouseY - camera.position.y ) * 0.05;
